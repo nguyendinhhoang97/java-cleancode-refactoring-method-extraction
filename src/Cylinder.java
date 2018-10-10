8a9ba8d67cc02e8f9ce6224fe2d01c9d7b70934c
HP Probook 6570b <dinhhoangvcu@gmail.com>
2018-10-10 11:32:29 +0700

    Practice method extraction

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,5 +1,5 @@
 public class Cylinder {
-    public static double getVolume(int radius, int height){
+    public static double getVolume(int radius, int height) {
         double baseArea = getBaseArea(radius);
         double perimeter = getPerimeter(radius);
         double volume = perimeter * height + 2 * baseArea;
@@ -7,7 +7,7 @@ public class Cylinder {
     }
 
     private static double getPerimeter(int radius) {
-        return 2 * Math.PI  * radius;
+        return 2 * Math.PI * radius;
     }
 
     private static double getBaseArea(int radius) {
